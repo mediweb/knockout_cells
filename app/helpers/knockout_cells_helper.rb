cf97ece31ffdc322ea7ae403e04af596e0a01cb4
Arek Turlewicz <aturlewicz@mediweb.jp>
2013-04-27 03:38:11 +0900

    Idea with passing block to methods in cell.

--- app/helpers/knockout_cells_helper.rb
+++ app/helpers/knockout_cells_helper.rb
@@ -1,4 +1,19 @@
 module KnockoutCellsHelper
+  def magic(&block)
+      render_cell :magic, :foo do |cell|
+        cell.inner_html = capture do
+          KnockoutForm.new(cell).instance_eval(&block) if block_given?
+        end
+      end
+  end
+  class KnockoutForm
+    def initialize(cell)
+      @cell = cell
+    end
+    def method_missing(method, *args)
+      @cell.render_state method
+    end
+  end
   def ko_context(options, &block)
     render_cell :context, :main, options, capture(&block)
   end
@@ -17,42 +32,5 @@ module KnockoutCellsHelper
 
   def ko_submit
     render_cell :context, :submit
-  end
-
-  class KnockoutForm
-    attr_accessor :custom_bindings
-    def initialize(view_context, options={})
-      @view_context = view_context
-      @options = options
-    end
-    def self.form_for(view_context, model, options={}, &block)
-      a = KnockoutForm.new(view_context, options)
-      a.form_for(model) do
-        yield(a)
-      end
-    end
-    def text_field(field_name, options={})
-      @view_context.render "shared/bootstrap/input", field_name: field_name, options: options, scope: @scope
-    end
-    def submit(field_name, options={})
-      @view_context.render "shared/bootstrap/submit", field_name: field_name, options: options
-    end
-    def fields_for(model=nil, &block)
-      @scope = model
-      @view_context.content_tag :div, "data-bind" => "with: $root.section($data,'#{model}',$data.data.#{model})".html_safe, &block
-    end
-    def select(field_name, options={})
-      @view_context.render "shared/bootstrap/select", field_name: field_name, options: options, scope: @scope
-    end
-    def form_for(model=nil, &block)
-      @scope = @options[:as]
-      content = @view_context.capture do
-        block.call(self)
-      end
-      @view_context.render "shared/bootstrap/form", model: model, content: content, options: @options
-    end
-  end
-  def ko_form_for(model, options={}, &block)
-    KnockoutForm.form_for(self, model, options, &block)
   end
 end
