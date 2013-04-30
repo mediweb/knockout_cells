34d3aa1ccd9021f84b3b5385382dd39d8840a9d4
Arek Turlewicz <aturlewicz@mediweb.jp>
2013-05-01 00:53:46 +0900

    Remove no longer needed helper methods.

--- app/helpers/knockout_cells_helper.rb
+++ app/helpers/knockout_cells_helper.rb
@@ -5,21 +5,4 @@ module KnockoutCellsHelper
   def ko_context(options, &block)
     render_cell :context, :main, options, capture(&block)
   end
-
-  def ko_input(opts)
-    render_cell :context, :ko_input do |c|
-      c.opts = opts
-    end
-  end
-
-  def ko_fields_for(options, &block)
-    render_cell :context, :fields_for do |c|
-      c.inner_html = capture(&block)
-      c.opts = options
-    end
-  end
-
-  def ko_submit
-    render_cell :context, :submit
-  end
 end
